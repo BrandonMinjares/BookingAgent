98f712b4897c3d5bb4e70ff3768a3726ccbd256d
Brandon Minjares <brandon.minjares@gmail.com>
2019-12-22 18:30:21 -0800

    Created Model for Band

---
+++ models/Bands.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+
+const BandsSchema = new mongoose.Schema({
+    user: {
+        // connects to an id that represents the user model
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: false,
+        maxlength: [30, 'Name can not be more than 30 charachters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [200, 'Description can not be more than 200 charachters']
+    },
+    website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTPS'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlength: [15, 'Phone number can not be longer than 15 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please add a valid email'
+        ]
+    },
+    location: {
+        type: {
+            type: String
+        },
+        formattedAddress: String,
+        city: String,
+        state: String,
+        country: String
+    },
+    genre: {
+        type: [String],
+        required: true,
+        enum: [
+            'Original Artist',
+            'Tribute'
+        ]
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'Rating must be at least 1'],
+        max: [10, 'Rating can not be more than 10']
+    },
+    averageCost: Number,
+    photo: {
+        type: String
+    }
+});
