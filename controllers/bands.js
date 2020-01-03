23db35eda0659cd97536a9084f52a08df1c95ed1
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-03 00:41:32 -0800

    Wrapped requests in  async/await which allowed us to delete try/catch

--- controllers/bands.js
+++ controllers/bands.js
@@ -1,23 +1,20 @@
 const ErrorResponse = require('../utils/errorResponse');
 const Band = require('../models/Bands');
+const asyncHandler = require('../middleware/async');
+
 
 // @desc    Get all bands
 // @route   GET /api/v1/bands
 // @access  Public
-exports.getBands = async (req, res, next) => {
-    try {
+exports.getBands = asyncHandler(async (req, res, next) => {
         const bands = await Band.find();
         res.status(200).json({ success: true, count: bands.length, data: bands});
-    } catch(err) {
-        next(err);
-    }
-};
+});
 
 // @desc    Get single band
 // @route   GET /api/v1/bands/:id
 // @access  Public
-exports.getBand = async (req, res, next) => {
-    try { 
+exports.getBand = asyncHandler(async (req, res, next) => {
         const band = await Band.findById(req.params.id);
 
         // If the ID is in the correct format but the band does not exist you get custom error message
@@ -26,34 +23,23 @@ exports.getBand = async (req, res, next) => {
         } 
 
         res.status(200).json({ success: true, data: band});
-    } catch(err) {
-        // If the ID is in the incorrect format you get custom error message
-        
-        next(err);
-        //next(new ErrorResponse(`Band not found with id of ${req.params.id}`, 404));
-    }
-};
+});
 
 // @desc    Create new band
 // @route   POST /api/v1/bands/
 // @access  Private
-exports.createBand = async (req, res, next) => {
-    try {
+exports.createBand = asyncHandler(async (req, res, next) => {
         const band = await Band.create(req.body);
         res.status(201).json({
             success: true,
             data: band
         });
-    } catch(err) {
-        next(err);
-    }
-};
+});
 
 // @desc    Update band
 // @route   PUT /api/v1/bands/:id
 // @access  Private
-exports.updateBand = async (req, res, next) => {
-    try {
+exports.updateBand = asyncHandler(async (req, res, next) => {
         const band = await Band.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
             runValidators: true
@@ -62,25 +48,18 @@ exports.updateBand = async (req, res, next) => {
             return next(new ErrorResponse(`Band not found with id of ${req.params.id}`, 404));
         } 
         res.status(200).json({ success: true, data: band })
-    } catch(err) {
-        next(err);
-    }
-};
+});
 
 // @desc    Delete band
 // @route   PUT /api/v1/bands/:id
 // @access  Private
-exports.deleteBand = async (req, res, next) => {
-    try {
+exports.deleteBand = asyncHandler(async (req, res, next) => {
         const band = await Band.findByIdAndDelete(req.params.id);
         if(!band) {
             return next(new ErrorResponse(`Band not found with id of ${req.params.id}`, 404));
         } 
         res.status(200).json({ success: true, data: {} })
-    } catch(err) {
-        next(err);
-    }
-};
+});
 
 
 // @desc    Get bands within a radius
