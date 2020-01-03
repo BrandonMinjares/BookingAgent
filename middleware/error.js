3f6cbc843a1cfe536eb8fb098b4cdeed05738393
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-02 19:37:28 -0800

    Error Handler added for duplicate values.

--- middleware/error.js
+++ middleware/error.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next) => {
 
     error.message = err.message;
     // Log to console for dev
-    console.log(err.stack);
+    console.log(err);
 
     // Mongoose bad ObjectID
     if(err.name === 'CastError') {
@@ -13,6 +13,11 @@ const errorHandler = (err, req, res, next) => {
         error = new ErrorResponse(message, 404);
     }
 
+    // Mongoose duplicate key
+    if(err.code === 11000) {
+        const message = `Duplicate field value entered`;
+        error = new ErrorResponse(message, 400);
+    }
 
     res.status(error.statusCode || 500).json({
         success: false,
