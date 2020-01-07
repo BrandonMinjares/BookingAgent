7994ce28629500b30dcc91952216014f6206e5bd
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-07 14:42:47 -0800

    Salt is generated and password can now be hashed.

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,5 +7,14 @@ const asyncHandler = require('../middleware/async');
 // @route   PUT /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async(req, res, next) => {
+    const { name, email, password } = req.body;
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password
+    });
+
     res.status(200).json({ success: true });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -33,6 +35,12 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         dafult: Date.now
     } 
+});
+
+// Encrypt passwrod using bcrypt
+UserSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', UserSchema);
