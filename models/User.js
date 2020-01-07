51164021792b03091597357b9e63d7e44c6c5149
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-07 13:01:29 -0800

    Added to User model

--- models/User.js
+++ models/User.js
@@ -3,16 +3,28 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: [true, 'Please add a name']
     },
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, 'Please add an email'],
+        unique: true,
+        match: [
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please add a valid email'
+        ]
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Please add a password'],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
     },
     avatar: {
         type: String
