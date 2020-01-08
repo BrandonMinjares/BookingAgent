a025b1c4e27eb679417390383365ec3bcbbcb2cd
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-08 14:48:12 -0800

    Secure flag on cookie becomes secure if app is in production.

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,9 +57,15 @@ const sendTokenResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken();
 
     const options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
         httpOnly: true
     };
+
+    if(process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
 
     res
         .status(statusCode)
