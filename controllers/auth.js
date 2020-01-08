646a5115efb4f284ceee3ef8c277df02f6be278f
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-08 12:40:40 -0800

    Added login routes and login credential verification

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middleware/async');
 
 
 // @desc    Register User
-// @route   PUT /api/v1/auth/register
+// @route   POST /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async(req, res, next) => {
     const { name, email, password } = req.body;
@@ -15,6 +15,43 @@ exports.register = asyncHandler(async(req, res, next) => {
         email,
         password
     });
+
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token: token });
+});
+
+
+// @desc    Login User
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async(req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email and password
+    if(!email || !password) {
+        return next(new ErrorResponse('Please provide email and password', 400));
+    }
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials. Either user does not exist or password is incorrect.', 401));
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials. Either user does not exist or password is incorrect.', 401));
+    }
+
+    // Important to make !user and !isMatch function return same message to ensure that the
+    // person who entered the information isn't sure whether the error was an incorrect email
+    // or an incorrect password -- Extra Security
+
+
 
     // Create token
     const token = user.getSignedJwtToken();
