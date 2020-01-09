2cf1392a930b0ff98a90c537469c86febe0e1867
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-08 19:55:59 -0800

    Created protect method to verify that a valid token was used to access certain routes.

--- routes/api/bands.js
+++ routes/api/bands.js
@@ -1,5 +1,10 @@
 const express = require('express');
+// Not sure yet
+const Band = require('../../models/Bands');
+
 const router = express.Router();
+
+const { protect } = require('../../middleware/auth');
 
 const { 
     getBands,
