ee8aa787b0eea46b75d7b404f8b12edd49e62c7e
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-10 14:05:35 -0800

    Successfully created registration form!

--- public/javascript/axios.js
+++ public/javascript/axios.js
@@ -1,12 +1,44 @@
 
 
-const registerForm = document.getElementById('registerForm');
+const registerForm = document.querySelector('.registerForm');
 
-const registerUser = () => {
-    alert('hey');
-    /*axios.get('/api/v1/users/').then(response => {
-        console.log(response);
-    });*/
+registerForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    registerUser(name, email, password);
+});
+
+const registerUser = async (name, email, password) => {
+    try {
+        const res = await axios({
+        method:'POST',
+        url: 'http://localhost:5000/api/v1/auth/register',
+        data: {
+            name,
+            email,
+            password
+        }
+    });
+        console.log(res);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
-registerForm.addEventListener('submit', registerUser);
+/*
+const registerUser = () => {
+    axios.post({
+        method: 'POST',
+        url: 'http://localhost:5000/api/v1/auth/register',
+        data: {
+            name: 'name',
+            email: 'email',
+            password: 'password'
+        }
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+}
+*/
