8f4afb9863c22b55dee9fedb8bdc2cb65f6973f3
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-10 01:12:52 -0800

    Still trying to get post request to work for register user.

--- public/javascript/register.js
+++ public/javascript/register.js
@@ -1,6 +1,17 @@
 
+
 document.getElementById('registerForm').addEventListener('click', registerUser());
 
 function registerUser() {
-    alert('HeyThere');
+    axios({
+        method: 'post',
+        url: 'http://localhost:5000/api/v1/users/',
+        data: {
+            name: 'name',
+            email: 'email',
+            password: 'password'
+        }
+    })
+    .then(res => showOutput(res))
+    .catch(err => console.log(err));
 }
