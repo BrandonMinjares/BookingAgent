9b5a81d09dc6cbac96185f1ad23ecfd774b1217f
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-10 14:45:28 -0800

    Created login form and login form script.

--- public/javascript/axios.js
+++ public/javascript/axios.js
@@ -1,9 +1,6 @@
-
-
 const registerForm = document.querySelector('.registerForm');
 
 registerForm.addEventListener('submit', e => {
-    e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
@@ -41,4 +38,37 @@ const registerUser = () => {
     .then(res => console.log(res))
     .catch(err => console.log(err));
 }
+*/
+
+
+
+
+
+// Login FORM
+const loginForm = document.querySelector('.loginForm');
+
+loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    loginUser(email, password);
+});
+
+/*
+const loginUser = async (email, password) => {
+    try {
+        const res = await axios({
+        method:'POST',
+        url: 'http://localhost:5000/api/v1/auth/login',
+        data: {
+            email,
+            password
+        }
+    });
+        console.log(res);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 */
