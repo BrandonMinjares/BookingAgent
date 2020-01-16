ab064afe50ae614a4ab80d46b2ba21d27b5be9d5
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-15 19:34:50 -0800

    Commented out unused routes.

--- server.js
+++ server.js
@@ -42,11 +42,11 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs");
 
 app.use('/', (req, res) => res.render('dashboard'));
-app.use('/api/users', (req, res) => res.send('home'));
+/* app.use('/api/users', (req, res) => res.send('home'));
 app.get('/signin', (req, res) => res.send('/signin'));
 app.use('/api/users', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/profile', require('./routes/api/profile')); */
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}`));
