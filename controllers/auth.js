90d3f8f2a61187b51f696176403f793f94e1345a
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-20 14:40:46 -0800

    Implemented reset password functionality. Created reset password token. Installed Crypto.js package.

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,6 +53,40 @@ exports.login = asyncHandler(async(req, res, next) => {
 });
 
 
+// @desc    Get current logged in User
+// @route   POST /auth/me
+// @access  Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+// @desc    Forgot Password
+// @route   POST /auth/forgotpassword
+// @access  Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findOne({email: req.body.email });
+
+    if(!user) {
+        return next(new ErrorResponse('There is no user with that email', 404));
+    }
+
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+    await user.save({ validateBeforeSave: false });
+    console.log(resetToken);
+
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
+
+
 // Get token from Model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
     // Create token
@@ -72,26 +106,3 @@ const sendTokenResponse = (user, statusCode, res) => {
         .cookie('token', token, options)
         .json({ success: true, token });
 };
-
-
-// @desc    Get current logged in User
-// @route   POST /auth/me
-// @access  Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-    const user = await User.findById(req.user.id);
-
-    res.status(200).json({
-        success: true,
-        data: user
-    });
-});
-
-
-exports.forgetPassword = (req, res, next) => {
-    
-};
-
-exports.resetPassword = (req, res, next) => {
-
-};
-
