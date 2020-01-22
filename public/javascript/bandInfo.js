60fee2857270588d17dfbf5fba083ddfd2470ca8
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-22 13:17:37 -0800

    Added button with event listener to bands page that will redirect to specific band that was clicked.

---
+++ public/javascript/bandInfo.js
@@ -0,0 +1,31 @@
+const bandInfo = document.querySelector('.bandInfo');
+    if(bandInfo) {
+        addEventListener('click', e => {
+            e.preventDefault();
+            alert('yo');
+        }); 
+}
+
+/*
+const registerUser = async (name, email, password) => {
+    try {
+        const res = await axios({
+        method:'POST',
+        url: 'http://localhost:5000/auth/register',
+        data: {
+            name,
+            email,
+            password
+        }
+    });
+
+    //res.redirect('/');
+    if (res.data.status === 'success') {
+        window.location.href = '/login';
+    }
+} catch (err) {
+        alert(err.response.data.message);
+    }
+};
+
+*/
