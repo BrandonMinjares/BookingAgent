43c86501b113d896791e3be9d5a11e20a237689f
Brandon Minjares <brandon.minjares@gmail.com>
2020-01-26 15:38:32 -0800

    Imported passport dependency

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
+
 const User = require('../models/User');
 
 // @route   GET /users
