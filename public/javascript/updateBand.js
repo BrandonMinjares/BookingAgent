339202281b21c851e92d32ff5a5f4be14da873c3
Brandon Minjares <brandon.minjares@gmail.com>
2020-02-05 23:56:05 -0800

    Added update band script to footer.

---
+++ public/javascript/updateBand.js
@@ -0,0 +1,10 @@
+function updateInfo() {
+    const bandName = document.getElementById("band-name").textContent = ' he';
+    const bandDescription = document.getElementById("band-description").textContent = ' he';
+    const bandGenre = document.getElementById("band-genre").textContent = ' he';
+    //alert(bandName + bandDescription + bandGenre);
+}
+
+
+// Maybe my clicking update we turn it into a form?? And then clicking done makes a put request 
+// to update band, then redirect back to the band page??
