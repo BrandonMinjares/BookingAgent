d7516b98738dac3e3b05ad9726cabcd20fb122bd
Brandon Minjares <brandon.minjares@gmail.com>
2020-02-09 09:37:58 -0800

    Modified update band function to replace whatever text was selected to edit in the show band page. Also created test index page to practice React.

--- public/javascript/updateBand.js
+++ public/javascript/updateBand.js
@@ -1,8 +1,15 @@
+function updateBand() {
+
+    document.getElementById('band-name').innerHTML = " ";
+}
+
+
 const updateBand = document.querySelector('.updateBand');
     if(filterForm) {
         addEventListener('submit', e => {
-            const name = document.getElementById('bandname').value;
-            update(name);
+            const name = document.getElementById('bandname').innerHTML = " ";
+
+            //update(name);
         }); 
 }
  
@@ -10,7 +17,7 @@ const update = async (name) => {
     try {
         const res = await axios({
         method:'POST',
-        url: 'http://localhost:5000/bands/:id',
+        url: `http://localhost:5000/bands/${name}`,
         data: {
             name
         }
